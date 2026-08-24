3dee52b9e90b7de6a5ae08af7f8f60e1d3991b45
wekesav450-tech <wekesav450@gmail.com>
2026-08-24 12:27:00 +0300

    fix: route all API endpoints through single Vercel function

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getApp } from '../server.js';
+import { getApp } from '../server.ts';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
