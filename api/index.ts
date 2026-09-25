474ca49c93d7ab06580fb69882f53f0f0e7a4c4b
wekesav450-tech <wekesav450@gmail.com>
2026-09-25 15:51:52 +0300

    fix: load bundled Express server in Vercel function

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getApp } from '../server';
+const { getApp } = require('../dist/server.cjs') as { getApp: () => Promise<any> };
 
 function restoreApiPath(req: VercelRequest) {
   const rawPath = req.query.path;
